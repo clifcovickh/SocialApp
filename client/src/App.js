aa118ee92beb9870484d1f043e8390f9937e0ad0
clifcovickh <clifcovickh@gmail.com>
2025-02-09 21:02:09 +0700

    Fixed everything up to episode 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,20 @@
-import './App.css';
-import axios from "axios";
-import React from 'react';
-import {useEffect, useState} from "react"; //biar tiap di jalanin pagenya langsung ke ambil smw get requestnya
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-
-  const [listOfPosts, setListOfPosts] = useState([]);//list containing all the posts that we receieve from the api req
-
-  useEffect(()=>{
-    axios.get("http://localhost:1515/posts").then((response)=>{
-      setListOfPosts(response.data); // contains the api request
-    }); //axios get biar ditampilin smw, get our api yang dibikin sebelumnya
-  }, []);
-  return (<div className="App">
-      {listOfPosts.map((value,key)=>{ //key = index ke berapa
-        return (
-          <div className='post'>
-            <div className='title'>{value.title}</div>
-            <div className='body'>{value.postText}</div>
-            <div className='footer'>{value.username}</div>
-        </div>
-      );
-    })}
-  </div>
+  return (
+    <div className="App">
+      <Router>
+        <Link to="/createpost"> Create A Post</Link>
+        <Link to="/"> Home Page</Link>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/createpost" exact component={CreatePost} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
